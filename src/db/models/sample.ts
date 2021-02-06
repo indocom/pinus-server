28e65adc357e6c3922bd9c972b2226734fb9609d
Ryo Armanda <ryoarmanda@rocketmail.com>
2021-02-06 16:18:10 +0800

    Add explanatory comments to sample model file

--- src/db/models/sample.ts
+++ src/db/models/sample.ts
@@ -1,5 +1,10 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
 import { Sequelize, DataTypes, Model, Optional, ModelCtor } from "sequelize";
 
+// This interface represents the attributes we have for each model instance, mapping
+// each attribute to their TypeScript data types.
+// Note that `id`, `createdAt`, and `updatedAt` is defined with an optional guard as it is
+// already handled by Sequelize and so we do not need to redefine it later on the model creation.
 interface SampleAttributes {
   id?: number;
   name: string;
@@ -8,18 +13,28 @@ interface SampleAttributes {
   updatedAt?: Date;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
+// Some attributes are optional in `build` and `create` calls.
+// One of these attributes is definitely `id`, as this will be auto-incremented
+// by the database itself.
 interface SampleCreationAttributes extends Optional<SampleAttributes, "id"> {}
 
+// This interface represents the model instance
 export interface SampleInstance extends Model<SampleAttributes, SampleCreationAttributes>, SampleAttributes {}
 
+// As the Sequelize instance is not available until when the project runs, we define what is called
+// a "model factory" function, which requires a Sequelize instance and defines the
+// model when called.
 export function SampleFactory(sequelize: Sequelize): ModelCtor<SampleInstance> {
   const Sample = sequelize.define<SampleInstance>(
+    // This is the model name
     "Sample",
+    // This is the model definition, which maps the attributes to their
+    // Sequelize data types
     {
       name: DataTypes.STRING,
       value: DataTypes.INTEGER,
     },
+    // This is the model options, having `underscored: true` is required
     { underscored: true }
   );
 
