1be8553811d18c73b11f26df115e8adfa68ce4f7
Daffa Fathani A <daffa26@gmail.com>
2021-03-07 21:36:09 +0800

    added Article model

---
+++ src/db/migrations/20210307131630-create-article.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Articles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      body: {
+        type: Sequelize.STRING
+      },
+      categoryId: {
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Articles');
+  }
+};
---
+++ src/db/models/article.ts
@@ -0,0 +1,35 @@
+import { Sequelize, DataTypes, Model, Optional, ModelCtor } from "sequelize";
+
+export interface ArticleAttributes{
+  id?:number,
+  userId: number,
+  title: string,
+  description: string,
+  body: string,
+  categoryId:number,
+  createdAt?:Date,
+  updatedAt?:Date
+}
+
+export interface ArticleCreationAttributes extends Optional<ArticleAttributes,"id">{}
+
+export interface ArticleInstance extends Model<ArticleAttributes, ArticleCreationAttributes>, ArticleAttributes {}
+
+type ArticleModel = ModelCtor<ArticleInstance>;
+
+export function ArticleFactory(sequelize: Sequelize): ArticleModel {
+  const Article = sequelize.define<ArticleInstance>(
+    "Article",
+    {
+      userId: DataTypes.INTEGER,
+      title: DataTypes.STRING,
+      description: DataTypes.STRING,
+      body: DataTypes.STRING,
+      categoryId:DataTypes.INTEGER,
+    },
+    { underscored: true }
+  );
+
+  return Article;
+}
+
